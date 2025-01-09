a473cfa55f08e092a627beb5a8de68f88ee4ca26
ernitoft <ernes.fuenzalida@alumnos.ucn.cl>
2025-01-09 10:51:40 -0300

    refactor: Change baseurl

--- src/app/api/agent.js
+++ src/app/api/agent.js
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { store } from "../store/store";
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://taller3-backend-k3lj.onrender.com";
 
 const responseBody = (response) => response.data;
 axios.defaults.withCredentials = true;
